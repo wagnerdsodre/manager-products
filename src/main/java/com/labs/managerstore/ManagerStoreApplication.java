78b6d29fa21142a7829752391684dc9eab0dc0c7
Wagner Sodré <wagnerdsodre@gmail.com>
2023-10-13 13:01:27 -0300

    Project started

---
+++ src/main/java/com/labs/managerstore/ManagerStoreApplication.java
@@ -0,0 +1,13 @@
+package com.labs.managerstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/labs/managerstore/ManagerStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labs.managerstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
