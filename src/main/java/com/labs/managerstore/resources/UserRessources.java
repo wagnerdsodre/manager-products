e92e1b3484ec5bd49f9f4d5ac6ed26fc7ba7b74a
Wagner Sodré <wagnerdsodre@gmail.com>
2023-10-13 21:45:14 -0300

    Added methods insert, update and delete

--- src/main/java/com/labs/managerstore/resources/UserRessources.java
+++ src/main/java/com/labs/managerstore/resources/UserRessources.java
@@ -1,13 +1,21 @@
 package com.labs.managerstore.resources;
 
+
+import java.net.URI;
+import java.net.http.HttpRequest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.labs.managerstore.models.User;
 import com.labs.managerstore.services.UserServices;
@@ -31,5 +39,26 @@ public class UserRessources {
 		return ResponseEntity.ok().body(obj);			
 	}
 	
+	@PostMapping
+	public ResponseEntity<User> insertUser(@RequestBody User user){
+		user = services.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).body(user);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> updateUser(
+			@PathVariable(value = "id") Long id,
+			@RequestBody User obj){
+		obj = services.update(id, obj);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Object> deleteUser(@PathVariable Long id){
+		services.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 
 }
--- src/main/java/com/labs/managerstore/services/UserServices.java
+++ src/main/java/com/labs/managerstore/services/UserServices.java
@@ -5,12 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PutMapping;
 
 import com.labs.managerstore.models.User;
 import com.labs.managerstore.repository.UserRepository;
-
-import jakarta.persistence.Entity;
 
 @Service
 public class UserServices {
@@ -26,5 +23,25 @@ public class UserServices {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();		
 	}	
+	
+	public User insert(User u) {
+		return repository.save(u);
+	}
+	
+	
+	public User update(Long id, User u) {
+		User entity = repository.getReferenceById(id);
+		entity.setName(u.getName());
+		entity.setAddress(u.getAddress());
+		entity.setEmail(u.getEmail());		
+		return repository.save(entity);		
+	}
+	
+	
+	public void delete(Long id) {
+		User entity = repository.getReferenceById(id);
+		repository.deleteById(entity.getId());		
+	}
+	
 
 }
